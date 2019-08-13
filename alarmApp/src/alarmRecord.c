c9b750560e38fc30c8c07f5f61b5336a583e942e
Klemen Vodopivec <klemen@vodopivec.org>
2019-08-12 21:10:53 -0400

    Update the license info

--- alarmApp/src/alarmRecord.c
+++ alarmApp/src/alarmRecord.c
@@ -1,9 +1,5 @@
 /*************************************************************************\
-* Copyright (c) 2007 UChicago Argonne LLC, as Operator of Argonne
-*     National Laboratory.
-* Copyright (c) 2002 The Regents of the University of California, as
-*     Operator of Los Alamos National Laboratory.
-* EPICS BASE is distributed subject to a Software License Agreement found
+* alarmRecord is distributed subject to a Software License Agreement found
 * in file LICENSE that is included with this distribution. 
 \*************************************************************************/
 
