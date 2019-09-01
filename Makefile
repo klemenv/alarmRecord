37d016fe2a3ea873da200b5a74723238cd5945e3
Klemen Vodopivec <klemen@vodopivec.org>
2019-09-01 16:48:41 -0400

    Build as standalone EPICS project, with a testing IOC

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 #Makefile at top of application tree
 TOP = .
 include $(TOP)/configure/CONFIG
-DIRS += configure stdApp iocBoot
-stdApp_DEPEND_DIRS  = configure
-iocBoot_DEPEND_DIRS = stdApp
+DIRS += src
+DIRS += testApp
+DIRS += iocBoot
 include $(TOP)/configure/RULES_TOP
---
+++ iocBoot/ioctest/st.cmd
@@ -0,0 +1,14 @@
+#!../../bin/linux-x86_64-debug/test
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/test.dbd"
+test_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+dbLoadRecords("db/test.db")
+
+cd "${TOP}/iocBoot/${IOC}"
