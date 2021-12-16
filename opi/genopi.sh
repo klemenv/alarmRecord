ddde677ec9627444984aefd6040d840bc4836eea
Klemen Vodopivec <klemen@vodopivec.org>
2021-12-16 16:47:13 -0500

    Compile-time argument to define number of supported links
    Generates .dbd and .bob files based on the ALARMREC_NLINKS variable in configure/CONFIG_SITE.

---
+++ opi/genopi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This is a helper script that generates the alarmRecord.opi file from its
+# template file and populates the fields for the assigned number of links.
+
+TEMPLATE_FILE=${1:-alarmRecord.tmpl.bob}
+NLINKS=${2:-10}
+
+# We're assuming single 'group' widget in .bob file
+sed -n '/.*group/,${p;/^  <\/widget>/q}' $TEMPLATE_FILE > alarmRecordLink.tmp
+orig_y=`grep -m1 "<y>" alarmRecordLink.tmp | sed 's/.*>\(.*\)<.*/\1/'`
+orig_height=`grep -m1 "<height>" alarmRecordLink.tmp | sed 's/.*>\(.*\)<.*/\1/'`
+
+# Expand the group as many time as there are links
+rm -f alarmRecordLinks.tmp
+for i in `seq $NLINKS`; do
+	let y=orig_y+i*orig_height+i*5
+	sed -e "s/\$(L)/$i/g" -e "0,/<y>$orig_y/s/<y>$orig_y/<y>$y/" alarmRecordLink.tmp >> alarmRecordLinks.tmp
+done
+
+# Plug the expanded groups to the main .bob file
+sed -e '/^.*group/,/^  <\/widget>/c%GROUP%' $TEMPLATE_FILE | sed -e '/^%GROUP%/r alarmRecordLinks.tmp' -e '/%GROUP/d'
+
+#rm -f alarmRecordLink.tmp alarmRecordLinks.tmp
--- src/gendbd.sh
+++ src/gendbd.sh
@@ -9,20 +9,169 @@
 NLINKS=${1:-10}
 
 rm -f O.Common/alarmRecordFieldBlock* O.Common/alarmRecordFields.dbd
-awk -v RS='}\n' -v ORS='}\n' -F'\n' '
-{
-close(out)
-out="O.Common/alarmRecordFieldBlock" NR
-print > out
-}' alarmRecordFields.tmpl
 
-for f in O.Common/alarmRecordFieldBlock*; do
-	for i in `seq $NLINKS`; do
-		sed "s/\$(I)/$i/g" $f >> O.Common/alarmRecordFields.dbd
-	done
+cat << EOF
+menu(alarmrecLINKTYPE) {
+	choice(alarmrecLINKTYPE_EXT_NC,"External PV Not Connected")
+	choice(alarmrecLINKTYPE_EXT,"External PV OK")
+	choice(alarmrecLINKTYPE_LOCAL,"Local PV")
+	choice(alarmrecLINKTYPE_CONST,"Constant/Disabled")
+}
+menu(alarmrecMINSEVR) {
+	choice(alarmrecMINSEVR_MINOR,"MINOR")
+	choice(alarmrecMINSEVR_MAJOR,"MAJOR")
+	choice(alarmrecMINSEVR_INVALID,"INVALID")
+}
+menu(alarmrecOVERRIDESEVR) {
+	choice(alarmrecOVERRIDESEVR_NOCHANGE,"NO_CHANGE")
+	choice(alarmrecOVERRIDESEVR_MINOR,"MINOR")
+	choice(alarmrecOVERRIDESEVR_MAJOR,"MAJOR")
+	choice(alarmrecOVERRIDESEVR_INVALID,"INVALID")
+}
+
+recordtype(alarm) {
+	include "dbCommon.dbd"
+	field(CTX,DBF_NOACCESS) {
+		prompt("Record Private")
+		special(SPC_NOMOD)
+		interest(4)
+		extra("struct alarmRecordCtx *ctx")
+	}
+	field(EN,DBF_MENU) {
+		prompt("Record alarm output enable")
+		special(SPC_MOD)
+		pp(TRUE)
+		initial("1")
+		menu(menuYesNo)
+	}
+	field(LCH,DBF_MENU) {
+		prompt("Enable latching mode")
+		pp(TRUE)
+		initial("1")
+		menu(menuYesNo)
+	}
+	field(CLR,DBF_UCHAR) {
+		prompt("Clear latched alarm")
+		special(SPC_MOD)
+		initial("1")
+		pp(TRUE)
+		menu(menuYesNo)
+	}
+	field(VAL,DBF_STRING) {
+		prompt("Current Value")
+		special(SPC_NOMOD)
+		asl(ASL0)
+		initial("OK")
+		size(40)
+	}
+
+	%#define ALARMREC_STR_LEN 40
+	%#define ALARMREC_NLINKS $NLINKS
+EOF
+
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(INP$i,DBF_INLINK) {
+		prompt("Input $i")
+		promptgroup(GUI_CALC)
+		special(SPC_MOD)
+		interest(1)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(TY$i,DBF_MENU) {
+		prompt("Link $i type")
+		special(SPC_NOMOD)
+		interest(1)
+		menu(alarmrecLINKTYPE)
+		initial("1")
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(STR$i,DBF_STRING) {
+		prompt("String $i")
+		promptgroup(GUI_OUTPUT)
+		asl(ASL0)
+		initial("%VAL%")
+		pp(TRUE)
+		size(40)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(EN$i,DBF_MENU) {
+		prompt("Enable link $i")
+		promptgroup(GUI_INPUTS)
+		pp(TRUE)
+		initial("1")
+		menu(menuYesNo)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(ACT$i,DBF_MENU) {
+		prompt("Link alarm active $i")
+		promptgroup(GUI_INPUTS)
+		special(SPC_NOMOD)
+		initial("0")
+		menu(menuYesNo)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(OSV$i,DBF_MENU) {
+		prompt("Override severity $i")
+		menu(alarmrecOVERRIDESEVR)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(MSV$i,DBF_MENU) {
+		prompt("Minimum severity $i")
+		promptgroup(GUI_ALARMS)
+		pp(TRUE)
+		interest(1)
+		menu(alarmrecMINSEVR)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(DBI$i,DBF_DOUBLE) {
+		prompt("Debounce interval $i")
+		initial("0.0")
+		pp(TRUE)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(DBC$i,DBF_LONG) {
+		prompt("Debounce count limit $i")
+		special(SPC_MOD)
+		initial("1")
+		pp(TRUE)
+	}
+EOF
+done
+for i in `seq $NLINKS`; do
+	cat << EOF
+	field(SEV$i,DBF_MENU) {
+		prompt("Link $i last severity")
+		special(SPC_NOMOD)
+		menu(menuAlarmSevr)
+	}
+EOF
 done
 
-sed "/\$(FIELDS)/{
-	s/\$(FIELDS)/%#define ALARMREC_NLINKS $NLINKS/
-	r O.Common/alarmRecordFields.dbd
-}" alarmRecord.tmpl > alarmRecord.dbd
+cat << EOF
+}
+EOF
